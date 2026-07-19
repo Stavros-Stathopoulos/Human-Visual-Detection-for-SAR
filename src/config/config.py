a88e6f17206ce8c7761d8ce6e619a29149a654f3
Stavros Stathopoulos <up1101069@ac.upatras.gr>
2026-07-19 16:30:32 +0300

    update: the config file with new variables

--- src/config/config.py
+++ src/config/config.py
@@ -1,8 +1,10 @@
 import os
 
 _ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
-_DATA_DIR = os.path.join(os.path.dirname(_ROOT_DIR), 'data', 'WiSARD')
-_IR_DIR = os.path.join(_DATA_DIR, '_IR_')
-_VIS_DIR = os.path.join(_DATA_DIR, '_VIS_')
-_PREFIX_IR = '__ir__'
-_PREFIX_VIS = '__vis__'
+_DERIVED_DIR = os.path.join(os.path.dirname(_ROOT_DIR), 'data', 'derived')
+_PROJECT_ROOT = os.path.dirname(_ROOT_DIR)
+_DATA_DIR = os.path.join(os.path.dirname(_ROOT_DIR), 'data', 'WiSARD_Multi_Modal_Sample')
+_IR_DIR = os.path.join(_DATA_DIR, '210417_MtErie_Enterprise_IR_0004')
+_VIS_DIR = os.path.join(_DATA_DIR, '210417_MtErie_Enterprise_VIS_0003')
+_PREFIX_IR = '210417_MtErie_Enterprise_IR_0004_'
+_PREFIX_VIS = '210417_MtErie_Enterprise_VIS_0003_'
